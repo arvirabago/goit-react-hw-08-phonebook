8158e91e4c9478e5d258573b5cc2393988d5863a
arvirabago <rabago.arvilyzadessa@gmail.com>
2024-05-03 00:53:11 +0800

    sign in js

---
+++ src/components/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from './SignIn'; // Adjust the path as necessary
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
